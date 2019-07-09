aa45a62843e4ac4b3644f694857cf82b4e275389
Sandra Triebel <nu76fet@kippis.bioinf.uni-jena.de>
2019-07-09 14:42:37 +0200

    Snakefile, config.yaml and create.py for long-read-only assebmlies

---
+++ nanopore/create.py
@@ -0,0 +1,42 @@
+#create folders for the output files from the nanopore data pipeline
+import os
+
+configfile = open('config.yaml', 'r').read()
+liste = configfile.split('\n')
+if liste[0][:5] == 'path:':
+    path = liste[0][7:len(liste[0]) - 1]
+    os.mkdir(path)
+    liste.pop(0)
+else:
+    path = os.getcwd()
+for i in range (len(liste)):
+    parameter = liste[i].split(':')
+    details = parameter[1]
+    parameter = parameter[0]
+    details = details.split(',')
+    details[0] = details[0][2:len(details[0])]
+    details[len(details) - 1] = details[len(details) - 1][0:len(details[len(details) - 1]) - 1]
+    if parameter != 'strains':
+        os.mkdir(path + '/' + parameter)
+    if parameter == 'strains':
+        strains = details
+    if parameter == 'preprocessing':
+        preprocessing = details
+    if parameter == 'assembly':
+        assembly = details
+    if parameter == 'polishing':
+        polishing = details
+    if parameter == 'quality':
+        quality = details
+for preprocesser in preprocessing:
+    os.mkdir(path + '/preprocessing' + '/' + preprocesser)
+    for bacteria in strains:
+        for assembler in assembly:
+            os.mkdir(path + '/assembly' + '/' + bacteria + '_' + preprocesser + '_' + assembler)
+            os.mkdir(path + '/postprocessing' + '/' + bacteria + '_' + preprocesser + '_' + assembler)
+for check in quality:
+    os.mkdir(path + '/quality' + '/' + check)
+    if check != 'nanoplot':
+        for bacteria in strains:
+            os.mkdir(path + '/quality' + '/' + check + '/' + bacteria)
+
