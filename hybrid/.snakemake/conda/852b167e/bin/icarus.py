94d45ded6488c31cfd85778a5454458369e0f8df
Sandra Triebel <nu76fet@mahlzeit.bioinf.uni-jena.de>
2019-07-22 12:41:24 +0200

    visualization of the Snakefiles

---
+++ hybrid/.snakemake/conda/852b167e/bin/icarus.py
@@ -0,0 +1,4 @@
+#!/data/prostlocal2/projects/st_mycoplasma_assembly/git/hybrid/.snakemake/conda/852b167e/bin/python
+# EASY-INSTALL-SCRIPT: 'quast==5.0.2','icarus.py'
+__requires__ = 'quast==5.0.2'
+__import__('pkg_resources').run_script('quast==5.0.2', 'icarus.py')
